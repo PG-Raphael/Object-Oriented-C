d1c7bc10733011ded51eb0e6bc92f53760429cb3
Unknown <whity2232@naver.com>
2018-08-22 16:50:12 -0400

    First Workshop For C++/ Graph

---
+++ WS01/tools.h
@@ -0,0 +1,13 @@
+/* -----------------------------------------------------------------------
+name : Seungmin Yun
+student No. :010820132
+section: IPC144SJJ
+----------------------------------------------------------------------*/
+#ifndef SICT_TOOLS_H
+#define SICT_TOOLS_H
+
+namespace sict {
+	int getInt(int min, int max);
+	int menu();
+};
+#endif
